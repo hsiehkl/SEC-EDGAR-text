a04787208a950e9ba8c0f987dc80e7782fa1583c
hsiehkl <st4465@gmail.com>
2020-11-12 01:22:23 +0100

    extracted notes

--- src/document.py
+++ src/document.py
@@ -11,6 +11,7 @@ import copy
 import os
 from abc import ABCMeta
 import multiprocessing as mp
+import re
 
 from .utils import search_terms as master_search_terms
 from .utils import args, logger
@@ -38,21 +39,37 @@ class Document(object):
         self.prepare_text()
         prep_time = time.process_time() - start_time
         file_name_root = metadata_master.metadata_file_name
+
+        section_search_terms_with_notes = []
         for section_search_terms in master_search_terms[form_type]:
+            section_name = section_search_terms['itemname']
+            if section_name != "Notes":
+                section_search_terms_with_notes.append(section_search_terms)
+            else:
+                max_n = self.get_note_n()
+                for i in range(2, max_n+1):
+                    new_section_search_terms = section_search_terms.copy()
+                    new_section_search_terms["itemname"] = f"Note{i}"
+                    search_pairs = section_search_terms[self.search_terms_type()]
+                    new_search_pairs = self.transfrom_note_search_pair(i, max_n, search_pairs)
+                    new_section_search_terms[self.search_terms_type()] = new_search_pairs
+                    section_search_terms_with_notes.append(new_section_search_terms)
+
+
+        for section_search_terms_with_note in section_search_terms_with_notes:
             start_time = time.process_time()
             metadata = copy.copy(metadata_master)
             warnings = []
-            section_name = section_search_terms['itemname']
+            section_name = section_search_terms_with_note['itemname']
             section_output_path = file_name_root + '_' + section_name
             txt_output_path = section_output_path + '_excerpt.txt'
             metadata_path = section_output_path + '_metadata.json'
             failure_metadata_output_path = section_output_path + '_failure.json'
 
-            search_pairs = section_search_terms[self.search_terms_type()]
+            search_pairs = section_search_terms_with_note[self.search_terms_type()]
             text_extract, extraction_summary, start_text, end_text, warnings = \
                 self.extract_section(search_pairs)
             time_elapsed = time.process_time() - start_time
-            # metadata.extraction_method = self.extraction_method
             metadata.section_name = section_name
             if start_text:
                 start_text = start_text.replace('\"', '\'')
@@ -63,12 +80,13 @@ class Document(object):
             metadata.time_elapsed = round(prep_time + time_elapsed, 1)
             metadata.section_end_time = str(datetime.utcnow())
             if text_extract:
+                # cleaned_text = self.remove_short_single_line(text_extract)
                 # success: save the excerpt file
-                metadata.section_n_characters = len(text_extract)
-                metadata.section_n_words = len(text_extract.split())
+                metadata.section_n_characters = len(cleaned_text)
+                metadata.section_n_words = len(cleaned_text.split())
                 with open(txt_output_path, 'w', encoding='utf-8',
                           newline='\n') as txt_output:
-                    txt_output.write(text_extract)
+                    txt_output.write(cleaned_text)
                 log_str = ': '.join(['SUCCESS Saved file for',
                                          section_name, txt_output_path])
                 self.log_cache.append(('DEBUG', log_str))
@@ -93,7 +111,48 @@ class Document(object):
                 metadata.save_to_db()
         return(self.log_cache)
 
+    def transfrom_note_search_pair(self, i, max_n, search_pairs):
+        pairs = []
+        for pair in search_pairs:
+            new_pair = pair.copy()
+            new_pair['start'] = pair['start'].replace('-', f'{i}')
+            if i != max_n:
+                new_pair['end'] = pair['end'].replace('-', f'{i+1}')
+            else:
+                new_pair['end'] = "\n\s(?:PART.{,40})?Item\s9.{,10}Changes\sin\sand\sDisagreements\sWith.{,99}?\n"
+            pairs.append(new_pair)
+        print(f"Note {i}")
+        print(new_pair)
+        return pairs
+
+    def get_note_n(self):
+        pattern =  'Note\s\d+'
+        note_search = re.findall(pattern,
+                            self.doc_text,
+                            re.DOTALL | re.IGNORECASE)
+        max_n = 0
+        for note in note_search:
+            try:
+                n = int(note.split(" ")[1])
+                if n > max_n:
+                    max_n = n
+            except Exception as e:
+                print(f"Failed to get note number with {note}. {e}")
+
+        return max_n
+
+    def remove_short_single_line(self, text):
+        print("Start to clean text!!!!!!")
+        orignal_text = text.copy()
+        pattern = '(^|\n).{0,30}(?=\n|$)'
+        print(pattern)
+        cleaned_text = re.sub(pattern, '\n', orignal_text, re.DOTALL | re.IGNORECASE)
+        print("XXXX-1")
+        cleaned_text = re.sub(r'\n+', '\n\n', cleaned_text).strip()
+        print("XXXX-2")
+        print(cleaned_text)
+        return cleaned_text
+
     def prepare_text(self):
         # handled in child classes
         pass
-
